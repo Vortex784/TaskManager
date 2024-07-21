e5b38e234579e86169a597d206d91906e96c62a9
Vortex784 <109910240+Vortex784@users.noreply.github.com>
2024-07-21 15:49:57 +0200

    New activity start

---
+++ app/src/main/java/com/example/taskmanager/ui/rewards/RewardItem.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.ui.rewards;
+
+public class RewardItem {
+    private String name;
+    private String description;
+    private int price;
+    private boolean singleUse;
+}
