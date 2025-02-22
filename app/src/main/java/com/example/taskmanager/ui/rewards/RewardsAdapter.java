151856053b516c21e2c90127aa99f6a10aba3039
Vortex784 <109910240+Vortex784@users.noreply.github.com>
2025-02-22 17:44:19 +0100

    Edit rewards

--- app/src/main/java/com/example/taskmanager/ui/rewards/RewardsAdapter.java
+++ app/src/main/java/com/example/taskmanager/ui/rewards/RewardsAdapter.java
@@ -2,19 +2,21 @@ package com.example.taskmanager.ui.rewards;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.taskmanager.R;
 import com.example.taskmanager.database.Points;
 import com.example.taskmanager.database.PointsDao;
 import com.example.taskmanager.database.TaskDatabase;
 import com.example.taskmanager.databinding.ItemRewardBinding;
-import com.example.taskmanager.ui.rewards.RewardItem;
-import com.example.taskmanager.ui.rewards.RewardItem;
-import com.example.taskmanager.database.TaskDatabase;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
@@ -28,7 +30,6 @@ public class RewardsAdapter extends RecyclerView.Adapter<RewardsAdapter.RewardVi
         this.rewardsList = rewardsList;
         this.context = context;
     }
-
 
     @NonNull
     @Override
@@ -57,21 +58,30 @@ public class RewardsAdapter extends RecyclerView.Adapter<RewardsAdapter.RewardVi
             super(binding.getRoot());
             this.binding = binding;
 
-            binding.btnComplete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
+            // Complete Reward
+            binding.btnComplete.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
                     RewardItem reward = rewardsList.get(position);
                     markAsComplete(reward);
                 }
             });
 
-            binding.btnDelete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
+            // Delete Reward
+            binding.btnDelete.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
                     RewardItem reward = rewardsList.get(position);
                     deleteReward(reward);
+                }
+            });
+
+            // Edit Reward
+            binding.btnEdit.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    RewardItem reward = rewardsList.get(position);
+                    editReward(reward);
                 }
             });
         }
@@ -83,52 +93,64 @@ public class RewardsAdapter extends RecyclerView.Adapter<RewardsAdapter.RewardVi
         }
 
         private void markAsComplete(RewardItem reward) {
-            new AsyncTask<Void, Void, Void>() {
+            new AsyncTask<Void, Void, Boolean>() {
                 @Override
-                protected Void doInBackground(Void... voids) {
+                protected Boolean doInBackground(Void... voids) {
                     TaskDatabase db = TaskDatabase.getInstance(context.getApplicationContext());
                     PointsDao pointsDao = db.pointsDao();
                     Points points = pointsDao.getPoints();
+
                     if (points == null) {
                         points = new Points(0);
                         pointsDao.insert(points);
                     }
+
                     if (points.getPoints() >= reward.getPrice()) {
                         points.setPoints(points.getPoints() - reward.getPrice());
                         pointsDao.update(points);
+
                         if (!reward.isRepeatable()) {
-                            deleteReward(reward);
+                            db.rewardDao().deleteReward(reward);
                         }
-                        Snackbar.make(binding.getRoot().getRootView(), "Reward claimed", Snackbar.LENGTH_LONG).show();
-                        return null;
+                        return true;
+                    } else {
+                        return false;
                     }
-                    Snackbar.make(binding.getRoot().getRootView(), "Not enough points", Snackbar.LENGTH_LONG).show();
-                    return null;
                 }
 
                 @Override
-                protected void onPostExecute(Void aVoid) {
-                    notifyDataSetChanged();
-
+                protected void onPostExecute(Boolean success) {
+                    if (success) {
+                        Snackbar.make(binding.getRoot(), "Reward claimed", Snackbar.LENGTH_LONG).show();
+                        notifyDataSetChanged();
+                    } else {
+                        Snackbar.make(binding.getRoot(), "Not enough points", Snackbar.LENGTH_LONG).show();
+                    }
                 }
             }.execute();
         }
 
+        private void editReward(RewardItem reward) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("reward_id", reward.getId()); // Pass reward ID
+
+            NavController navController = Navigation.findNavController((View) binding.getRoot().getParent());
+            navController.navigate(R.id.nav_edit_reward, bundle); // Navigate to EditRewardFragment
+        }
+
         private void deleteReward(RewardItem reward) {
-            // Remove the reward from the database
             new AsyncTask<RewardItem, Void, Void>() {
                 @Override
                 protected Void doInBackground(RewardItem... rewards) {
-                    TaskDatabase.getInstance(itemView.getContext()).rewardDao().deleteReward(rewards[0]);
-                    Snackbar.make(binding.getRoot().getRootView(), "Reward Deleted", Snackbar.LENGTH_LONG).show();
+                    TaskDatabase.getInstance(context.getApplicationContext()).rewardDao().deleteReward(rewards[0]);
                     return null;
                 }
 
                 @Override
                 protected void onPostExecute(Void aVoid) {
-                    // Remove the reward from the list and notify the adapter
                     rewardsList.remove(reward);
                     notifyDataSetChanged();
+                    Snackbar.make(binding.getRoot(), "Reward Deleted", Snackbar.LENGTH_LONG).show();
                 }
             }.execute(reward);
         }
